26cfca3dfcb2347704d2eee3b1c0f34596d9bc3d
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-26 13:02:05 -0400

    Canvas is the size of the window

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
 
 function setup() {
-    createCanvas(600,600);
+    createCanvas(windowWidth - 20,windowHeight - 20);
 }
 
 function draw() {
