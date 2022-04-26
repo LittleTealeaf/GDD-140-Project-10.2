dd4d090ad2a410e7e6a5039fa1519399adb10502
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-04-26 18:14:56 +0000

    deploy: 8b3b7937bf041d666d28ddc6d13a56450cd20e3b

--- sketch.js
+++ sketch.js
@@ -1,9 +1,36 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
 
+const playerSpeed = 4;
+
+let groupEntity;
+let groupWall;
+
+let player;
+
 function setup() {
     createCanvas(windowWidth - 20,windowHeight - 20);
+
+    groupEntity = new Group();
+    groupWall = new Group();
+
+    player = createSprite(width/2,height/2,30,30);
+    groupEntity.add(player);
+
+    groupWall.add(createSprite(width/3,height/3,40,40));
 }
 
 function draw() {
     background(220);
+
+    groupEntity.collide(groupWall,(a,b) => a.displace(b));
+
+
+    playerMovement();
+    drawSprites();
+}
+
+
+function playerMovement() {
+    player.velocity.x = keyDown('a') ? -playerSpeed : keyDown('d') ? playerSpeed : 0;
+    player.velocity.y = keyDown('w') ? -playerSpeed : keyDown('s') ? playerSpeed : 0;
 }
