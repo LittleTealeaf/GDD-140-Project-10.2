d98a5ba08373ee77597d2f893b4413f662058222
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-04-29 13:10:54 +0000

    deploy: 0b2e7052074ae6ffd58d6665cc283ead43c90715

--- sketch.js
+++ sketch.js
@@ -1,44 +1,56 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
 
+/*
+
+Use the WSAD keys to move around. The goal is to collect 15 of the green points (ther are more points than required to compensate for some being unreachable). However, avoid the red squares or they will killy you!
+*/
+
+
 const playerSpeed = 4;
+const pointsCount = 20;
+const pointsToWin = 15;
 
-let groupEntity;
-let groupWall;
-let groupEnemy;
-let groupGoal;
+var points = 0;
+var lives = 3;
 
-let player;
+var groupEntity;
+var groupWall;
+var groupEnemy;
+var groupGoal;
+
+var player;
 
 function setup() {
-    createCanvas(windowWidth - 20,windowHeight - 20);
+    createCanvas(windowWidth - 20, windowHeight - 20);
 
     groupEntity = new Group();
     groupWall = new Group();
     groupEnemy = new Group();
     groupGoal = new Group();
 
-    player = createSprite(width/2,height/2,15,15);
+    player = createSprite(width / 2, height / 2, 15, 15);
     player.friction = 0.1;
+    player.shapeColor = color('blue');
     groupEntity.add(player);
 
-    for(var i = 0; i < 150; i++) {
-        const s = createSprite(random(width),random(height),40,40);
+    for (var i = 0; i < 150; i++) {
+        const s = createSprite(random(width), random(height), 40, 40);
         s.shapeColor = color('grey');
         groupWall.add(s);
     }
 
-    for(var i = 0; i < 15; i++) {
-        const rot = random(0,360);
-        const dist = random(width/4,width/2);
-        const s = createSprite(dist * cos(rot) + width / 2,dist * sin(rot) + height / 2,30,30);
+    for (var i = 0; i < 15; i++) {
+        const rot = random(0, 360);
+        const dist = random(width / 4, width / 2);
+        const s = createSprite(dist * cos(rot) + width / 2, dist * sin(rot) + height / 2, 30, 30);
         s.friction = 0.1;
         s.shapeColor = color('red');
         groupEntity.add(s);
         groupEnemy.add(s);
     }
 
-    for(var i = 0; i < 5; i++) {
-        const s = createSprite(random(width),random(height),20,20);
+    for (var i = 0; i < pointsCount; i++) {
+        const s = createSprite(random(width), random(height), 20, 20);
         s.shapeColor = color('green');
         groupGoal.add(s);
         groupEntity.add(s);
@@ -49,19 +61,50 @@ function setup() {
 function draw() {
     background(220);
 
-    groupEntity.collide(groupWall,(a,b) => a.displace(b));
+    textAlign(LEFT,TOP);
 
-    groupEnemy.toArray().forEach((item) => {
-        item.attractionPoint(0.1,player.position.x,player.position.y);
-    });
+    if (lives > 0 && points < pointsToWin) {
+        groupEntity.collide(groupWall, (a, b) => a.displace(b));
 
-    groupEnemy.collide(player, (a,b) => {
-        
-    });
+        groupEnemy.toArray().forEach((item) => {
+            item.attractionPoint(0.1, player.position.x, player.position.y);
+        });
+
+        groupEnemy.collide(player, (a, b) => {
+            lives--;
+            a.remove();
+        });
+
+        groupGoal.collide(player, (a, b) => {
+            points++;
+            a.remove()
+        });
 
 
-    playerMovement();
-    drawSprites();
+        playerMovement();
+        drawSprites();
+
+        textSize(30);
+        text("Points: " + points + "/" + pointsToWin, 0, 0);
+        text("Lives: " + lives, 0,30);
+    } else {
+        textSize(30);
+        textAlign(CENTER,CENTER);
+        if(points < pointsToWin) {
+            text("YOU LOSE, HIT SPACE TO PLAY AGAIN",width/2,height/2);
+        } else {
+            text("YOU WIN! PRESS SPACE TO PLAY AGAIN",width/2,height/2);
+        }
+
+        if(keyDown('space')) {
+            lives = 3;
+            points = 0;
+            while(allSprites.length > 0) {
+                allSprites[0].remove();
+            }
+            setup();
+        }
+    }
 }
 
 
